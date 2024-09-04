2d8782ec5ec1a98635be5a6149fe58278a533986
Christie <christie.tanc@gmail.com>
2024-09-04 15:49:35 +0800

    website functionality

---
+++ script.js
@@ -0,0 +1,286 @@
+let data=[
+    {
+        "ID": "000",
+        "Name": {
+          "Scientific_Name": "",
+          "Common_Names": []
+        },
+        "Locale": ["WACEN"],
+        "Plant_Family": "Broadleaf",
+        "Geography": { "desc": "", "image": "", "source": "" },
+        "Growing": { "desc": "", "image": "", "source": "" },
+        "Image": "",
+        "Description": "",
+        "Edibility": {
+          "Edible": true,
+          "Edible_Parts": {
+            "Flower": true,
+            "Stem": true,
+            "Leaf": true,
+            "Root": true,
+            "Source": ""
+          }
+        },
+        "Further_Reading": ["", "", ""]
+      },
+    {
+        "ID": "001",
+        "Name": {
+        "Scientific_Name": "Sonchus oleraceus",
+        "Common_Names": ["common sowthistle", "sow thistle", "smooth sow thistle", "annual sow thistle", "hare's colwort", "hare's thistle", "milky tassel", "milk thistle", "soft thistle"]
+        },
+        "Locale": ["WACEN"],
+        "Plant_Family": "Broadleaf",
+        "Geography": { "desc": "Widely distributed throghout West Australia usually growing in waste places and on disturbed ground.", "image": "./sources/geography/G001.png", "source": "https://avh.ala.org.au/occurrences/search?taxa=sow+thistle#tab_mapView" },
+        "Growing": { "desc": "Spring to Autumn", "image": "./sources/seasons/S001.png", "source": "https://avh.ala.org.au/occurrences/search?taxa=sow+thistle#tab_chartsView" },
+        "Image": "./sources/thumbnails/I001.jfif",
+        "Description": "Sonchus oleraceus is a species of flowering plant in the tribe Cichorieae of the family Asteraceae, native to Europe and Western Asia",
+        "Edibility": {
+        "Edible": true,
+        "Description":"All parts of the plant can be eaten. From root to leaves, to flowers",
+        "Edible_Parts": {
+            "Flower": true,
+            "Stem": true,
+            "Leaf": true,
+            "Root": true,
+            "Source": "https://www.eatweeds.co.uk/sowthistle-sonchus-spp#habitat"
+        }
+        },
+        "Further_Reading": ["https://en.wikipedia.org/wiki/Sonchus_oleraceus","https://www.eatweeds.co.uk/sowthistle-sonchus-spp", "https://www.eatweeds.co.uk/sowthistle-recipe", "https://www.eattheweeds.com/sonchus-sow-thistle-in-a-pigs-eye-2/"]
+    },
+    {
+        "ID": "002",
+        "Name": {
+        "Scientific_Name": "Lactuca serriola",
+        "Common_Names": ["prickly lettuce", "milk thistle", "compass plant", "scarole"]
+        },
+        "Locale": "WACEN",
+        "Plant_Family": "Broadleaf",
+        "Geography": { "desc": "Lactuca serriola is native to Europe, Asia, and north Africa, and has become naturalized elsewhere", "image": "./sources/geography/G001.png", "source": "https://avh.ala.org.au/occurrences/search?q=taxa%3A%22prickly+lettuce%22#tab_mapView" },
+        "Growing": { "desc": "Spring to Autumn", "image": "./sources/seasons/S002.png", "source": "https://avh.ala.org.au/occurrences/search?q=taxa%3A%22prickly+lettuce%22#tab_chartsView" },
+        "Image": "./sources/thumbnails/I002.jpg",
+        "Description": "(not to be confused with Silybum marianum, also called milk thistle) It has a slightly fetid odor and is commonly considered a weed of orchards, roadsides and field crops. It is the closest wild relative of cultivated lettuce (Lactuca sativa L.).",
+        "Edibility": {
+        "Edible": true,
+        "Description":"Lactuca serriola can be eaten as a salad, although it has something of a bitter taste. Young leaves can be eaten raw or cooked. Older leaves can be steamed. While unsubstantial, its roots have been used as a coffee substitute.",
+        "Edible_Parts": {
+            "Flower": true,
+            "Stem": true,
+            "Leaf": true,
+            "Root": true,
+            "Source": "https://foragerchef.com/wild-lettuce/"
+        }
+        },
+        "Further_Reading": ["https://en.wikipedia.org/wiki/Lactuca_serriola","https://foragerchef.com/wild-lettuce/", "https://wildfoodgirl.com/2013/let-us-appreciate-wild-lettuce/"]
+    },
+    {
+      "ID": "003",
+      "Name": {
+        "Scientific_Name": "Stellaria media",
+        "Common_Names": ["common chickweed", "chickenwort", "craches", "maruns", "winterweed"]
+      },
+      "Locale": ["WACEN"],
+      "Plant_Family": "Broadleaf",
+      "Geography": { "desc": "While found across WA, chickweed is most commonly seen in the South-West", "image": "./sources/geography/G003.png", "source": "https://avh.ala.org.au/occurrences/search?q=taxa%3A%22chickweed%22#tab_mapView" },
+      "Growing": { "desc": "Growing season for chickweed is considered in the winter months", "image": "./sources/seasons/S003.png", "source": "https://avh.ala.org.au/occurrences/search?q=taxa%3A%22chickweed%22#tab_chartsView" },
+      "Image": "./sources/thumbnails/I003.jpg",
+      "Description": "chickweed, is an annual flowering plant in the family Caryophyllaceae. It is native to Eurasia and naturalized throughout the world, where it is a weed of waste ground, farmland and gardens. It is sometimes grown as a salad crop or for poultry consumption.",
+      "Edibility": {
+        "Edible": true,
+        "Description":"Stellaria media is edible and nutritious, and is used as a leaf vegetable, often raw in salads. It is also eaten by chickens, wild birds, and mountain sheep. However, it is not advised for pregnant and breastfeeding mothers.",
+        "Edible_Parts": {
+          "Flower": true,
+          "Stem": true,
+          "Leaf": true,
+          "Root": false,
+          "Source": ""
+        }
+      },
+      "Further_Reading": ["https://www.eattheweeds.com/chickweed-connoisseurs-2/", "https://www.gaiasorganicgardens.com.au/chickweed-stelaria-media/#:~:text=Grows%20only%20seasonally%20in%20Perth,areas%20of%20loose%20moist%20soil.", "https://plantura.garden/uk/herbs/chickweed/chickweed-overview"]
+    }
+]
+let currentEntryID= '000';
+function setEntry(ID){
+currentEntryID = ID;
+alert(ID);
+}
+function getEntryByID(ID){
+
+    for (let i = 0; i < data.length; i++)
+        {
+            if(ID == data[i].ID){
+                return data[i];
+            }
+        }
+}
+function updatePage(ID){
+    let entry = getEntryByID(ID);
+    document.getElementById('main-image').src = entry.Image;
+    document.getElementById('weed-name').innerText = capitalizeFirstLetter(entry.Name.Common_Names[0]);
+    document.getElementById('scientific-name').innerText = entry.Name.Scientific_Name;
+    document.getElementById('other-names').innerText += ` ${entry.Name.Common_Names}`;
+    document.getElementById('description').innerText = entry.Description;
+
+    document.getElementById('edible-desc').innerText = entry.Edibility.Description;
+    document.getElementById('flower-check').classList.replace("fa-check", returnSymbol(entry.Edibility.Edible_Parts.Flower));
+    document.getElementById('stem-check').classList.replace("fa-check", returnSymbol(entry.Edibility.Edible_Parts.Stem));
+    document.getElementById('leaves-check').classList.replace("fa-check", returnSymbol(entry.Edibility.Edible_Parts.Leaf));
+    document.getElementById('roots-check').classList.replace("fa-check", returnSymbol(entry.Edibility.Edible_Parts.Root));
+
+    document.getElementById('geography-fig').children[0].src = entry.Geography.image;
+    document.getElementById('geography-fig').children[1].innerText += ` ${entry.Geography.source}`;
+    document.getElementById('geography-desc').innerText = entry.Geography.desc;
+
+    document.getElementById('season-fig').children[0].src = entry.Growing.image;
+    document.getElementById('season-fig').children[1].innerText += ` ${entry.Growing.source}`;
+    document.getElementById('season-desc').innerText = entry.Growing.desc;
+
+    document.getElementById('further-reading').innerText = entry.Further_Reading;
+}
+
+function getEntryByID(ID){
+
+    for (let i = 0; i < data.length; i++)
+        {
+            if(ID == data[i].ID){
+                return data[i];
+            }
+        }
+}
+
+function returnSymbol(bool){
+    if(bool === true){
+        return 'fa-check'
+    }
+    else{
+        return 'fa-xmark'
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function generateGalleryItem(){
+    for (let i = 0; i < data.length; i++) {
+        if(i !== 0)
+        document.getElementById('gallery-population').innerHTML += 
+        `<div class="gallery-item">
+            <a target="_blank" onclick="createNewPage(${data[i].ID})">
+            <img src=${data[i].Image} alt=${data[i].Name.Common_Names[0]}>
+            <div class="desc">${data[i].Name.Common_Names[0]}</div>
+            </a>
+        </div>  `;
+    }
+}
+
+function createNewPage(ID) { 
+    const newWindow = window.open(ID, '_blank'); // Open a new window/tab 
+    newWindow.document.write(` 
+        <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eat the Weeds WA | Entry</title>
+    <link rel="stylesheet" href="./component_styles.css">
+    <link rel="stylesheet" href="./generic_styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <script>
+      let pageID = ${ID};
+    </script>
+  </head>
+  <body onload="updatePage(pageID)">
+    <nav>
+        <span class="brand-logo">
+          <i class="fa-solid fa-seedling"></i>
+            <h3>Eat the Weeds WA</h3>
+        </span>
+        <ul>
+            <li><a class="navButton" href="./index.html">Home</a></li>
+            <p class="collapse-margins">|</p>
+            <li><a class="navButton" href="./browse.html">Gallery</a></li>
+            <p class="collapse-margins">|</p>
+            <li><a class="navButton" href="./search.html">Search</a></li>
+        </ul>
+    </nav>
+    <main class="entry-body">
+    <div class="section entry-hero collapse-top-margin collapse-top-padding">
+        <div class="page-margin">
+            <div class="img-block">
+            <img id="main-image" src="./sources/thumbnails/I001.jfif" alt="">
+            </div>
+            <h1 id="weed-name" class="collapse-bottom-margin">Common Sowthistle</h1>
+            <h4 id="scientific-name" class="collapse-bottom-margin">Sonchus oleraceus, Broadleaf</h4>
+            <h5 id="other-names" class="collapse-bottom-margin"><b>Also called:</b></h5>
+            <p id="description" class="collapse-bottom-margin">Sonchus oleraceus is a species of flowering plant in the tribe Cichorieae of the family Asteraceae, native to Europe and Western Asia</p>
+        </div>
+    </div>
+    <div class="section entry-edibility collapse-top-padding">
+        <div class="page-margin">
+        <h2 class="collapse-bottom-margin">Edible Parts</h2>
+        <p id="edible-desc">Sonchus oleraceus is a species of flowering plant in the tribe Cichorieae of the family Asteraceae, native to Europe and Western Asia</p>
+        <table class="edibility-table collapse-bottom-margin"">
+            <tr>
+              <td>Flowers</td>
+              <td><i id="flower-check" class="fa-solid fa-check"></i></td>
+            </tr>
+            <tr>
+              <td>Stem</td>
+              <td><i id="stem-check" class="fa-solid fa-check"></i></td>
+            </tr>
+            <tr>
+              <td>Leaves</td>
+              <td><i id="leaves-check" class="fa-solid fa-check"></i></td>
+            </tr>
+            <tr>
+              <td>Roots</td>
+              <td><i id="roots-check" class="fa-solid fa-check"></i></td>
+            </tr>
+          </table>
+        </div>
+    </div>
+    <div class="section entry-geo-growth collapse-top-padding">
+        <div class="page-margin">
+        <h2>Geography and Growth</h2>
+        <figure id="geography-fig">
+            <img src="./sources/geography/G001.png" alt="chart of georaphic availability">
+            <figcaption>source:</figcaption>
+          </figure>
+        <p id="geography-desc">Sonchus oleraceus is a species of flowering plant in the tribe Cichorieae of the family Asteraceae, native to Europe and Western Asia</p>
+
+        <figure id="season-fig">
+            <img src="./sources/seasons/S001.png" alt="chart of seasonal availability">
+            <figcaption>source:</figcaption>
+          </figure>
+        <p id="season-desc">Sonchus oleraceus is a species of flowering plant in the tribe Cichorieae of the family Asteraceae, native to Europe and Western Asia</p>
+    </div>
+    </div>
+    <div class="section entry-futher-reading collapse-top-padding">
+        <div class="page-margin">
+        <h2>Further Reading</h2>
+        <ul id="further-reading">
+            <li>species of flowering plant in the tribe</li>
+            <li>species of flowering plant in the tribe</li>
+            <li>species of flowering plant in the tribe</li>
+            <li>species of flowering plant in the tribe</li>
+        </ul>
+        <div class="button-shelf">
+            <a href="./browse.html"><button class="primary-button">Back to Gallery</button></a>
+        </div>
+    </div>
+    </div>
+    </main>
+    <footer class="collapse-margins">
+        <p>Copyright Christie Tanjaya 2024</p>
+    </footer>
+    <script src="https://kit.fontawesome.com/4cc7121f30.js" crossorigin="anonymous"></script>
+    <script src="./script.js"></script>
+  </body>
+</html>
+    `);
+    newWindow.document.close(); // Close the document to render the content 
+} 
